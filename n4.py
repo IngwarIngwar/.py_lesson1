723ff755df1d08d51b38eeef5e8011cb163a6674
ingwar <turuyut@gmail.com>
2021-12-21 01:28:01 +0300

    n1

---
+++ n4.py
@@ -0,0 +1,10 @@
+num_input = int(input('Введите целое положительное число - '))
+num1 = int(num_input) % 10
+num2 = int(num_input) // 10
+num3 = int(num2) % 10
+num4 = int(num2) // 10
+num5 = int(num4) % 10
+while num_input > 0:
+    digit = num1
+
+        break
